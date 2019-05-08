7d5be80e53a385b7c7a8ea730c6ed0d15d34042b
Mat Lowery <mlowery@ebay.com>
2019-05-08 11:40:39 -0600

    misc

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -180,7 +180,7 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 #defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 # Disable press-and-hold for keys in favor of key repeat
-#defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 2
